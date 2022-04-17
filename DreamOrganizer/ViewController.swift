5ee3c2c301a4213c4c5ea8c01ffb9bb96961da25
Julio Cesar <juliocesar@Julios-MacBook-Pro.local>
2022-04-17 01:19:21 -0300

    Initial layout created

---
+++ DreamOrganizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DreamOrganizer
+//
+//  Created by Julio Cesar  on 16/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
